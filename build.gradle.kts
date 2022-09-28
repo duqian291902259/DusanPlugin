ddd495294cddf82f2bcf041d0be76c5c0f59b02f
杜小菜 <duqian2010@gmail.com>
2022-09-29 06:59:34 +0800

    【Plugin】1.0.5,优化插件说明

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,10 @@ plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.6.20"
     id("org.jetbrains.intellij") version "1.5.2"
-    //id("org.jetbrains.intellij") version "0.6.5"
 }
 
 group = "site.duqian.plugin"
-version = "1.0.4"
+version = "1.0.5"
 
 repositories {
     mavenCentral()
@@ -25,12 +24,10 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     //version.set("2021.2")
-    //version.set("2021.2.1")
     version.set("2022.1.1") //2020.1 Could not resolve com.jetbrains:jbre:jbr_jcef-11_0_8-osx-aarch64-b944.34.
     type.set("IC") // Target IDE Platform
     //type.set("AI") // Target Android Studio Platform
 
-    //plugins.set(listOf())
     plugins.set(listOf("android"))
 }
 
@@ -38,16 +35,8 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
     implementation("org.freemarker:freemarker:2.3.31")
 
-    // https://mvnrepository.com/artifact/com.hynnet/DJNativeSwing-SWT
-    //implementation("com.hynnet:DJNativeSwing-SWT:1.0.0")
-    //implementation("chrriis:DJNativeSwing-SWT:+")
-
-    // https://mvnrepository.com/artifact/org.eclipse.platform/org.eclipse.swt.win32.win32.x86_64
-    //implementation("org.eclipse.platform:org.eclipse.swt.win32.win32.x86_64:+")
-
     // https://mvnrepository.com/artifact/org.eclipse.platform/org.eclipse.swt.cocoa.macosx.x86_64
     //implementation("org.eclipse.platform:org.eclipse.swt.cocoa.macosx.x86_64:3.121.0")
-
 }
 
 tasks {
@@ -57,14 +46,14 @@ tasks {
         targetCompatibility = "11"
         options.encoding = "UTF-8"
     }
-    /*withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+   /* withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
     }*/
 
     patchPluginXml {
         //sinceBuild.set("212")
         sinceBuild.set("202")
-        //untilBuild.set("222.*")
+        untilBuild.set("260.*")
     }
 
    /* signPlugin {
