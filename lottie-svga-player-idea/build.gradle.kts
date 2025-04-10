b59d1305e81b04fef21b5a8dd7663afecfc84646
杜乾 <duqian2010@gmail.com>
2025-04-10 13:52:46 +0800

    【Plugin】upgrade plugin version 251.*

--- lottie-svga-player-idea/build.gradle.kts
+++ lottie-svga-player-idea/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "site.duqian.plugin"
-version = "1.3.3"
+version = "1.3.4"
 repositories {
     mavenCentral()
     google()
@@ -51,7 +51,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("182")
-        untilBuild.set("243.*")
+        untilBuild.set("251.*")
     }
 
    /* signPlugin {
